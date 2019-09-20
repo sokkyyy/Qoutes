c2091ebed7e9acd063ae9383d09f2e3ded6cd6a6
sokkyyy <ndegwaraymond@gmail.com>
2019-09-20 12:54:25 +0300

    Qoute Form Validation

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote('All that we are is the result of what we have thought', 'Buddha', 'Ray', 11, 0),
-    new Quote('I have no special talent. I am only passionately curious', 'Albert Einstein', 'Ray', 20, 3),
-    new Quote('If you judge people, you have no time to love them', 'Mother Teresa', 'Ray', 30, 14),
-    new Quote('Wisely, and slow. They stumble that run fast', 'William Shakespeare', 'Ray', 6, 3),
+    new Quote(1, 'All that we are is the result of what we have thought', 'Buddha', 'Ray', 11, 0),
+    new Quote(2, 'I have no special talent. I am only passionately curious', 'Albert Einstein', 'Ray', 20, 3),
+    new Quote(3, 'If you judge people, you have no time to love them', 'Mother Teresa', 'Ray', 30, 14),
+    new Quote(4, 'Wisely, and slow. They stumble that run fast', 'William Shakespeare', 'Ray', 6, 3),
   ];
 
   showDetails(index) {
@@ -31,6 +31,11 @@ export class QuoteComponent implements OnInit {
     if (confirmDelete) {
       this.quotes.splice(index, 1);
     }
+  }
+  submitQuote(quote) {
+    quote.id = this.quotes.length + 1;
+    this.quotes.push(quote);
+    console.log(this.quotes);
   }
 
 
